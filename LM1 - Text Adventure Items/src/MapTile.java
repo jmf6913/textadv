e77dfb5900d7659a512172504c58526f5311b9b9
Joshua Ferrell <123476499+jmf6913@users.noreply.github.com>
2023-03-12 23:36:25 -0400

    Update MapTile.java

--- LM1 - Text Adventure Items/src/MapTile.java
+++ LM1 - Text Adventure Items/src/MapTile.java
@@ -4,6 +4,9 @@ public class MapTile{
 
     public Method adjacent_positions;
     public MapTile(int x, int y) {
+        Scanner scanner = new Scanner(System.in);
+        int xCord = scanner.nextInt();
+        int yCord = scanner.nextInt();
         this.x = x;
         this.y = y;
     }
