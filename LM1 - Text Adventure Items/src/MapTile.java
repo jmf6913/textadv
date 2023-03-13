c5658c31f69e039c28fab6a7372515184eeec54e
Joshua Ferrell <jmf6913>
2023-03-12 20:17:32 -0400

    MapTile - Used to track the movements and positions of the player on the map

---
+++ LM1 - Text Adventure Items/src/MapTile.java
@@ -0,0 +1,11 @@
+public class MapTile{
+    public static int x;
+    public static int y;
+
+    public Method adjacent_positions;
+    public MapTile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
